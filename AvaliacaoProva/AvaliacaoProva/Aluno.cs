9324a423d1c749a2c43de4eb5ed0e59aa675242f
Luiz Henrique Dutra <luizh.dutra001@outlook.com>
2026-04-16 01:05:49 -0300

    Commit AvaliacaoProva

---
+++ AvaliacaoProva/AvaliacaoProva/Aluno.cs
@@ -0,0 +1,26 @@
+﻿using System.Globalization;
+
+namespace AvaliacaoProva {
+    internal class Aluno {
+        public string Nome;
+        public double[] Nota = new double[3];
+
+        private double NotaFinal() {           
+            return Nota[0] + Nota[1] + Nota[2];
+        }       
+
+        public override string ToString() {
+             if(NotaFinal() >= 60.00) {
+                return "NOTA FINAL = "
+                    + NotaFinal().ToString("F2",CultureInfo.InvariantCulture)
+                    + "\nAPROVADO";
+            }
+            else {
+                return "NOTA FINAL = "
+                    + NotaFinal().ToString("F2",CultureInfo.InvariantCulture)
+                    + "\nREPROVADO"
+                    + "\nFALTARAM " + (60.00 - NotaFinal()).ToString("F2", CultureInfo.InvariantCulture) + " PONTOS";
+            }
+        }
+    }
+}
---
+++ AvaliacaoProva/AvaliacaoProva/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Globalization;
+
+namespace AvaliacaoProva {
+    class Program {
+        static void Main(string[] args) {
+            Aluno aluno = new Aluno();
+
+            do {
+                Console.Write("Digite o nome do aluno: ");
+                aluno.Nome = Console.ReadLine();
+
+                if(String.IsNullOrWhiteSpace(aluno.Nome) || aluno.Nome.Any(char.IsDigit)){
+                    Console.WriteLine("\nO nome do aluno é obrigatório e não pode conter números!");
+                }
+            } while(String.IsNullOrWhiteSpace(aluno.Nome) || aluno.Nome.Any(char.IsDigit));
+
+            Console.WriteLine("Digite as três notas do aluno: ");
+
+            //Primeira nota
+            do {                 
+                aluno.Nota[0] = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+                if(aluno.Nota[0] < 0.00 || aluno.Nota[0] > 30.00) {
+                    Console.WriteLine("A primeira nota deve ser maior ou igual a zero e menor ou igual a 30.Digite novamente: ");
+                }
+            } while(aluno.Nota[0] < 0.00 || aluno.Nota[0] > 30.00);
+
+            //Segunda e terceira notas
+            for(int i = 1;i <= 2;i++) {
+                do {
+                    aluno.Nota[i] = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+                    if(aluno.Nota[i] < 0.00 || aluno.Nota[i] > 35.00) {
+                        Console.WriteLine("A segunda e a terceira notas devem ser maiores ou iguais a zero e menores ou iguais a 35.Digite novamente:");
+                    }
+                } while(aluno.Nota[i] < 0.00 || aluno.Nota[i] > 35.00);
+            }            
+
+            Console.WriteLine(aluno);
+        }                 
+    }
+}
