53268d9ac399c3f406b55cc13c09c9cc0a67a5c5
Luiz Henrique Dutra <luizh.dutra001@outlook.com>
2026-04-22 23:03:57 -0300

    commit Cotacao

---
+++ Cotacao/Cotacao/ConversorMoeda.cs
@@ -0,0 +1,9 @@
+﻿namespace Cotacao {
+    internal static class ConversorMoeda {
+        public static double Iof = 0.06;
+
+        public static double ConversaoDolar(double cotacaoDolar,double valor) {
+            return (valor + valor * Iof) * cotacaoDolar;
+        }
+    }
+}
---
+++ Cotacao/Cotacao/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+
+namespace Cotacao {
+    class Program {
+        static void Main(string[] args) {
+            double cotacaoDolar, valor;
+
+            do {
+                Console.Write("Qual é a cotação do dólar (entre com números maiores que zero)? ");
+            } while(!double.TryParse(Console.ReadLine(),CultureInfo.InvariantCulture,out cotacaoDolar) || cotacaoDolar <= 0.00);
+
+            do {
+                Console.Write("Quantos dólares você vai comprar (entre com números maiores que zero)? ");
+            } while(!double.TryParse(Console.ReadLine(), CultureInfo.InvariantCulture, out valor) || valor <= 0.00);
+
+            double conversao = ConversorMoeda.ConversaoDolar(cotacaoDolar, valor);
+
+            Console.WriteLine($"Valor a ser pago = R${conversao.ToString("F2", CultureInfo.InvariantCulture)}");
+        }
+    }
+}
